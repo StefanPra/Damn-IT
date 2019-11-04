47fa054d175bc0c4aafdea9189584642d8803bdf
Stefan Näslund <56830149+StefanPra@users.noreply.github.com>
2019-11-04 13:17:03 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Damn_IT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Helddo World!");
         }
     }
 }
